eab9e2888f8b33157a27ee3127f62811d49a755d
Cyprien G <cyprien.guillemot@gmail.com>
2015-05-12 23:30:00 +0200

    Start design of add book page

--- assets/js/divasma.js
+++ assets/js/divasma.js
@@ -1,6 +1,15 @@
-//Init bootstrap tooltip
 $(function () {
+  //Init bootstrap tooltip
   $('[data-toggle="tooltip"]').tooltip()
+
+  $("#data_book #image_input").change(function(){
+    var url = $("#data_book #image_input").val();
+    $("<img>", {
+      src: url,
+      error: function() { $("#data_book .image-group").addClass("has-error"); },
+      load: function() { $("#data_book #image").attr("src", url); }
+    });
+  });
 })
 
 /* Show a notification
