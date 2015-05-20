963ab459f580364f173d424bbd8bad2217ce8348
Cyprien G <cyprien.guillemot@gmail.com>
2015-05-20 18:23:00 +0200

    Final add book design

--- assets/js/divasma.js
+++ assets/js/divasma.js
@@ -2,13 +2,24 @@ $(function () {
   //Init bootstrap tooltip
   $('[data-toggle="tooltip"]').tooltip()
 
+  //Image previsualization in add book form
   $("#data_book #image_input").change(function(){
     var url = $("#data_book #image_input").val();
+
+    //Test image
     $("<img>", {
       src: url,
       error: function() { $("#data_book .image-group").addClass("has-error"); },
       load: function() { $("#data_book #image").attr("src", url); }
     });
+  });
+
+  //Show/Hide the original book form in add book form
+  $("#show_hide_translation").change(function(){
+    if($("#show_hide_translation").is(":checked"))
+      $("#data_book .translation_data").css("display", "block");
+    else
+      $("#data_book .translation_data").css("display", "none");
   });
 })
 
